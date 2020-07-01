10a178561ee65849d215ecf95bf153bc4f29b19a
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-07-01 12:08:01 +0530

    Add files via upload

---
+++ Module 1/M1T4E/M1T4E4_code_Challenge_sol/M01_T04_E04_Code_challenge_Solution (1).R
@@ -0,0 +1,29 @@
+# Code challenge
+# Module 1: Getting Started
+
+
+# Solution:
+
+# 1) Sum of 45 and 321
+45 + 321
+
+# 2) Difference between 23 and 9
+23 - 9
+
+# 3) 3 multiplied by 5 and divided by 2
+3 * 5 / 2
+
+# 4) 121 modulos 22 (remainder of the division)
+121 %% 22
+
+# 5) 12 to the 4th power
+12^4
+
+# 6) Difference between 3rd power of 17 and 3rd power of 15
+17^3 - 15^3
+
+# 7) Any expression that includes all six arithmetic operators, 
+# addition (+), subtraction (-), multiplication (*), division (/), 
+# modulos operator (%%) and power (^)
+(20 * 3 + 50) / (100 - 200%%23 + 5^2)  # just one example
+
